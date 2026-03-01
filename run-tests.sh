9acc6eaa531adfdde3a6709f09c6953d012658db
Jim Gray <JimGray9999@users.noreply.github.com>
2026-02-28 19:03:35 -0500

    Add request delay to prevent rate limiting

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Load .env file
 set -a
 source .env
 set +a
 
-# Run Newman
 newman run collections/ReqRes\ Full\ Suite.postman_collection.json \
   --iteration-data data/users.csv \
   --env-var "baseUrl=$REQRES_BASE_URL" \
   --env-var "apiKey=$REQRES_API_KEY" \
   --env-var "token=" \
+  --delay-request 500 \
   --reporters cli,htmlextra \
-  --reporter-htmlextra-export reports/report.html
+  --reporter-htmlextra-export reports/report.html \
+  --bail
